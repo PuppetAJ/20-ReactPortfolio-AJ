60859cf98d0057bfe2b367b815f5fc8f9edde06e
Adrian Jimenez <adrianj.web@gmail.com>
2025-01-18 19:55:49 -0500

    Removed outdated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,18 +8,38 @@ function Resume() {
   // Animation check effect on component load
   useEffect(() => {
     animCheck();
-  })
+  });
 
   // JSX
   return (
-    <section id='Resume' className = 'content-format'>
-      <Text className='section-header ease-anim' id='resume-header' fontSize='6xl' fontWeight = 'bold' mb='20px'>Resume</Text>
+    <section id='Resume' className='content-format'>
+      <Text
+        className='section-header ease-anim'
+        id='resume-header'
+        fontSize='6xl'
+        fontWeight='bold'
+        mb='20px'
+      >
+        Resume
+      </Text>
       {/* Resume embed */}
-      <object className='ease-anim resume-embed' type='application/pdf' data={require('../../assets/PDF/AdrianJResume.pdf')} >  <p>Your web browser doesn't have a PDF plugin.
-        Instead you can click<a className='pdf-dl' href={require('../../assets/PDF/AdrianJResume.pdf')} download> here
-        </a> to download the PDF file.</p></object>
+      <object
+        className='ease-anim resume-embed'
+        type='application/pdf'
+        data={require('')}
+      >
+        {' '}
+        <p>
+          Your web browser doesn't have a PDF plugin. Instead you can click
+          <a className='pdf-dl' href={require('')} download>
+            {' '}
+            here
+          </a>{' '}
+          to download the PDF file.
+        </p>
+      </object>
     </section>
-  )
+  );
 }
 
 // Export component
